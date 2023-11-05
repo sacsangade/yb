e719359465268c6ea27018dcbffd01eac0902c47
sacsangade <sacsangade@gmail.com>
2023-11-06 03:33:51 +0530

    Add files via upload

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+	 <Head>
+	 	<link href="https://fonts.cdnfonts.com/css/helvetica-neue-55" rel="stylesheet"/>
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+      </Head>
+	  
       <body>
         <Main />
         <NextScript />
